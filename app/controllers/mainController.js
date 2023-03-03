53b8a8834976bfec1b00851e56b985b5d9734ad7
DUJARDIN-Mathieu <111589638+DUJARDIN-Mathieu@users.noreply.github.com>
2023-03-03 18:32:24 +0100

    Update mainController.js

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,7 +1,7 @@
 const mainController = {
     homePage: async (req, res) => {
         try {
-            res.render('main_page')
+            res.render('mainPage')
         } catch (error) {
             console.log(error);
             res.status(500).send(`An error occured with the database :\n ${error.message}`)
