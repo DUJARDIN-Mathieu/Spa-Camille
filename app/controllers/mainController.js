f393c5ecd6d187e59f9864204e1623c417ba1197
DUJARDIN-Mathieu <ledocteur59@live.fr>
2023-03-04 12:51:06 +0100

    changement de nom

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,7 +1,15 @@
 const mainController = {
     homePage: async (req, res) => {
         try {
-            res.render('main_page')
+            res.render('homePage')
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(`An error occured with the database :\n ${error.message}`)
+        }
+    },
+    reservationPage: async (req, res) => {
+        try {
+            res.render('reservationPage')
         } catch (error) {
             console.log(error);
             res.status(500).send(`An error occured with the database :\n ${error.message}`)
--- app/router.js
+++ app/router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const mainController = require('./controllers/mainController')
 
 router.get('/', mainController.homePage)
+router.get('/reservez', mainController.reservationPage)
 
 module.exports = router
