e5c70a435fba2326803d795430944dbefbe46816
DUJARDIN-Mathieu <ledocteur59@live.fr>
2023-03-29 20:50:45 +0200

    prix total

--- BDD/database.js
+++ BDD/database.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
+    query: {
+        "unix_sock": "/cloudsql/{}/.s.PGSQL.5432".format(process.env.CLOUD_SQL_CONNECTION_NAME)
+    },
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT
